da7ca281b93e7cea1e09a01ebac8dab7b43f447d
Anna <anna.guegan@orange.fr>
2022-10-14 16:16:59 +0200

    UNE CHENILLEE

--- express_webapp/routes/users.js
+++ express_webapp/routes/users.js
@@ -9,11 +9,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req, res, next) => {
+    uq = true;
     user_dao.findAll(function(rows) {
         for (let i=0; i < rows.length;i++) {
-            console.log(rows[i].email);
+            if (rows[i].email == req.body.email){
+                uq = false;
+            };
         };
     });
+    if(uq){
+        user_dao.insert([req.body.nom,req.body.prenom,req.body.dateDeNaissance,req.body.sexe,req.body.taille,req.body.poids,req.body.email,req.body.motDePasse], () => {});
+    };
+    res.render('users',{});
 });
 
 module.exports = router;
