3278e7a49b9b1266d7824432936bc3564b6fb52c
Enzo <nzomorvan@gmail.com>
2022-10-14 15:23:33 +0200

    👌 IMPROVE

--- express_webapp/routes/connect.js
+++ express_webapp/routes/connect.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var user_dao = require('sport-track-db').user_dao;
 
 router.get('/', function(req, res, next) {
-    user_dao.findAll(function(rows) {
-            res.render('connect', {data:rows});
-    });
+    res.render('connect');
+    // user_dao.findAll(function(rows) {
+    //         res.render('connect', {data:rows});
+    // });
 });
 module.exports = router;
