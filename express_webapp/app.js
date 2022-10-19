88149079721a5e8ac17b1c4caa345242950d720b
HeineZo <aypierre30@gmail.com>
2022-10-19 23:04:31 +0200

    👌 IMPROVE: Bouton qui marche

--- express_webapp/app.js
+++ express_webapp/app.js
@@ -13,6 +13,7 @@ const connect = require('./routes/connect');
 const upload = require('./routes/upload');
 const valid = require('./routes/valid');
 const disconnect = require('./routes/disconnect');
+const update = require('./routes/user_update');
 
 
 
@@ -43,6 +44,7 @@ app.use('/connect', connect);
 app.use('/upload', upload);
 app.use('/valid', valid);
 app.use('/disconnect', disconnect);
+app.use('/update', update);
 // app.use(fileupload());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ express_webapp/routes/user_update.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var user_dao = require('sport-track-db').user_dao;
+
+router.get('/', function(req, res, next) {
+        
+        res.render('user_update', {
+            user:
+                "prenom": });
+    });
+});
+
+router.post('/', (req, res, next) => {
+    // uq = true;
+    // user_dao.findAll(function(rows) {
+    //     for (let i=0; i < rows.length; i++) {
+    //         if (rows[i].email == req.body.email){
+    //             uq = false;
+    //         };
+    //     };
+    // });
+    // if(uq){
+    //     user_dao.insert([req.body.nom,req.body.prenom,req.body.dateDeNaissance,req.body.sexe,req.body.taille,req.body.poids,req.body.email,req.body.motDePasse], (userId) => {
+    //         req.session.user = {
+    //             id: userId,
+    //             prenom: req.body.prenom,
+    //             register: true
+    //         };
+    //         res.redirect('/valid');
+    //     });
+    // } else {
+    //     res.redirect('/users');
+    // };
+});
+
+module.exports = router;
